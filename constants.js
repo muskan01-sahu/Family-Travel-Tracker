43f6832370c83c4a468741e7013a2a8a7cb2875a
muskan01-sahu <muskansahu069@gmail.com>
2026-01-17 16:21:39 +0530

    first commit

---
+++ constants.js
@@ -0,0 +1,9 @@
+export const port_app = 3000;
+
+export const user_DB = "postgres";
+export const host_DB = "localhost";
+export const database_DB = "world";
+export const password_DB = "Muskan@18";
+export const port_DB = 5432;
+
+   
